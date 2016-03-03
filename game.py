af7d1bfc9c14bb3ea94edac2bd983e1d307ac9c5
Robert <robert.schulz-95@web.de>
2016-03-03 20:48:36 +0100

    add addPlayer and removePlayer methods

--- game.py
+++ game.py
@@ -1,5 +1,3 @@
-import json
-
 class Game(object):
 
 	def __init__(self, player_name, side):
@@ -21,3 +19,33 @@ class Game(object):
 		
 		resultString = '{"game": {"gameid" : "' + self.gameid + '","team" : [{"blue" : {"player" :[{"name" :"'+ self.team_blue_player_name1+'"},{"name" :"'+ self.team_blue_player_name2+'"}], "goals" : "'+self.team_blue_goals+'"}},{"red"  : {"player" :[{"name" :"'+self.team_red_player_name1+'"},{"name" :"'+self.team_red_player_name2+'"}], "goals" : "'+self.team_red_goals+'"}}]}}'
 		return resultString
+
+	def addPlayer(self, player_name, side):
+		
+		if side == 'blue' and ( self.team_blue_player_name1 != ' ' or self.team_blue_player_name2 != ' '):
+			if self.team_blue_player_name1 == ' ':
+				self.team_blue_player_name1 = player_name
+			else:
+				self.team_blue_player_name2 = player_name
+		elif side == 'red' and ( self.team_red_player_name1 != ' ' or self.team_red_player_name2 != ' '):
+                        if self.team_red_player_name1 == ' ':
+                                self.team_red_player_name1 = player_name
+                        else:
+                                self.team_red_player_name2 = player_name
+# 		else:
+#			self.error_message = 'Team is already full'
+	def removePlayer(self, player_name, side):
+		 if side == 'blue': 
+			if self.team_blue_player_name2 == player_name:
+				self.team_blue_player_name2 = ' '
+                        if self.team_blue_player_name1 == player_name:
+                                self.team_blue_player_name1 = ' '
+                 if side == 'red':
+                        if self.team_red_player_name2 == player_name:
+                                self.team_red_player_name2 = ' '
+                        if self.team_red_player_name1 == player_name:
+                                self.team_red_player_name1 = ' '
+	
+
+ 
+		
